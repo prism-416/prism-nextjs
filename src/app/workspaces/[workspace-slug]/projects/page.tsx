a2121eca3fdc490eac519b154131b85ba4a6fe8a
lighteko <dev.lighteko@gmail.com>
2026-04-23 03:09:41 +0900

    feat(project): initial layout for projects

--- src/app/workspaces/[workspace-slug]/projects/page.tsx
+++ src/app/workspaces/[workspace-slug]/projects/page.tsx
@@ -1,10 +1,18 @@
+import MainLayout from "@/atomics/templates/MainLayout";
 import { ProjectsContent } from "@/domains/projects/components/ProjectsContent";
+import { AppHeader } from "@/domains/workspace/components/AppHeader";
+import AppSidebar from "@/domains/workspace/components/AppSidebar";
 import { Suspense } from "react";
 
 export default function ProjectsPage() {
   return (
-    <Suspense>
-      <ProjectsContent />
-    </Suspense>
+    <MainLayout
+      header={<AppHeader />}
+      sidebar={<AppSidebar />}
+    >
+      <Suspense>
+        <ProjectsContent />
+      </Suspense>
+    </MainLayout>
   );
 }
